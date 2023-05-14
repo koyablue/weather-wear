fa23a1f91c939976af3cb2db27b78cff042c9380
koyaaoyama <aoyama.ko0920@gmail.com>
2023-05-14 14:37:47 -0700

    src/utils/geolocation.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
 export const GEOLOCATION_API_URL = process.env.NEXT_PUBLIC_GEOLOCATION_API_ENDPOINT || ''
-export const geolocationApiKey = process.env.GEOLOCATION_API_KEY || ''
+export const GEOLOCATION_API_KEY = process.env.GEOLOCATION_API_KEY || ''
 // export connst geolocation
--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -1,4 +1,4 @@
-import { geolocationApiKey, GEOLOCATION_API_URL } from '@/constants/constants'
+import { GEOLOCATION_API_KEY, GEOLOCATION_API_URL } from '@/constants/constants'
 
 /**
  * Returns geolocation API url with params
@@ -8,5 +8,5 @@ import { geolocationApiKey, GEOLOCATION_API_URL } from '@/constants/constants'
  */
 export const getGeolocationApiUrl = (fields?: string[]): string => {
   const fieldsParam = fields && fields.length ? fields.join(',') : ''
-  return `${GEOLOCATION_API_URL}${geolocationApiKey}&fields=${fieldsParam}`
+  return `${GEOLOCATION_API_URL}${GEOLOCATION_API_KEY}&fields=${fieldsParam}`
 }
