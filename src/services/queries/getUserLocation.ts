aeaf3c90d9d0af57f7cae539942ac9fbeb24c774
koyaaoyama <aoyama.ko0920@gmail.com>
2023-05-15 10:40:05 -0700

    Fix getUserLocation. remove cach no-store option

--- src/services/queries/getUserLocation.ts
+++ src/services/queries/getUserLocation.ts
@@ -8,20 +8,12 @@ import { GEOLOCATION_CACHE_LIFETIME } from '@/constants/constants'
 import { getGeolocationApiEndpoint } from '@/utils/geolocation'
 
 /**
- * Geolocation API call
+ * Call Geolocation API
  *
  * @param {string[]} [fields]
  * @return {*}  {Promise<Partial<GeolocationApiResponse>>}
  */
 export const getUserLocation = async (fields?: string[]): Promise<Partial<GeolocationApiResponse>> => {
-  const res = await fetch(
-    getGeolocationApiEndpoint(fields),
-    {
-      cache: 'no-store',
-      next: {
-        revalidate: GEOLOCATION_CACHE_LIFETIME,
-      }
-    }
-  )
+  const res = await fetch(getGeolocationApiEndpoint(fields), { next: { revalidate: GEOLOCATION_CACHE_LIFETIME }})
   return res.json()
 }
