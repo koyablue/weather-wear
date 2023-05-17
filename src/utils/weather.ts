daf58287f696a85cde2de5a2f6b7c35e03abb94e
koyaaoyama <aoyama.ko0920@gmail.com>
2023-05-16 21:48:00 -0700

    Add utils/geocoding. Move getGeocodingApiEndpoint from utils/weather to utils/geocoding

--- src/utils/weather.ts
+++ src/utils/weather.ts
@@ -23,18 +23,3 @@ export const getCurrentWeatherApiEndpoint = (lat: number, lon: number) => (
 export const get5Day3hourForecastApiEndpoint = (lat: number, lon: number) => (
   `${WEATHER_API_BASE_ENDPOINT}/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}`
 )
-
-/**
- * https://openweathermap.org/api/geocoding-api
- *
- * https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
- *
- * @param {{ cityName: string, stateCode?: string, countryCode?: string }} q
- * @param {number} limit
- * @return {*} string
- */
-export const getGeocodingApiEndpoint = (
-q: { cityName: string, stateCode?: string, countryCode?: string }, limit: number) => {
-  const qString = `${q.cityName},${q.stateCode},${q.countryCode}`
-  return `${GEOCODING_API_ENDPOINT}?q=${qString}&limit=${limit}&appid=${OPEN_WEATHER_API_KEY}`
-}
