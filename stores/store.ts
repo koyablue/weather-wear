95a159d7cee610b570b350ed15cb0ed3501e0d1f
koyaaoyama <aoyama.ko0920@gmail.com>
2023-05-22 21:39:20 -0700

    Install redux-toolkit and do initial configuration

---
+++ stores/store.ts
@@ -0,0 +1,11 @@
+// https://redux-toolkit.js.org/tutorials/typescript
+
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {},
+})
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
