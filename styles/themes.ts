ae86c7f819a33f5d82478b67ed8a433eda5f9fe0
koyaaoyama <aoyama.ko0920@gmail.com>
2023-05-25 22:19:05 -0700

    Add darkTheme to styles/themes.ts

--- styles/themes.ts
+++ styles/themes.ts
@@ -1,6 +1,15 @@
-export const lightTheme = {
+import { ColorThemeStyle } from '../types/colorTheme'
+
+export const lightTheme: ColorThemeStyle = {
   colors: {
     text: '#333333',
     background: '#faf5eb',
   },
-} as const
+}
+
+export const darkTheme: ColorThemeStyle = {
+  colors: {
+    text: '#F2F2F2',
+    background: '#333333',
+  },
+}
