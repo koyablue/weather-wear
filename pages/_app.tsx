9274ccefe482248d32c467ffe822c1439058249c
koyaaoyama <aoyama.ko0920@gmail.com>
2023-05-28 12:20:02 -0700

    Fix pages/_app.tsx. Remove getInitialProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,26 +2,22 @@
 import { ReactElement, ReactNode } from 'react';
 
 // next
-import { NextPage, NextPageContext } from 'next';
-import type { AppContext, AppProps } from 'next/app'
+import { NextPage } from 'next';
+import type { AppProps } from 'next/app'
 
 // libraries
 import { ThemeProvider } from 'styled-components'
 import { Provider } from 'react-redux'
 
-// redux
+// stores
 import { store } from '../stores/store'
 
+// components
 import GlobalStyle from '../components/globalstyles'
 
-import { colorThemes } from '../constants/colorTheme';
-
-// hooks
 import { useColorTheme } from '../hooks/useColorTheme';
-import { getColorThemeCookie } from '../utils/cookie/colorTheme';
-import { ColorTheme, isColorTheme } from '../types/colorTheme';
-import { useAppDispatch, useAppSelector } from '../stores/hooks';
-import { selectColorTheme, updateColorTheme } from '../stores/slices/colorThemeSlice';
+
+import { ColorTheme } from '../types/colorTheme';
 
 // Layout configuration doc
 // https://nextjs.org/docs/pages/building-your-application/routing/pages-and-layouts#with-typescript
@@ -35,10 +31,6 @@ type AppPropsWithLayout = AppProps & {
   initialColorTheme: ColorTheme
 }
 
-// interface MyAppProps extends AppProps {
-//   initialTheme: object;
-// }
-
 /**
  * Takes a component to be wrapped
  * Return the component wrapped by ThemeProvider
@@ -46,30 +38,12 @@ type AppPropsWithLayout = AppProps & {
  * @param {NextPageWithLayout} Component
  * @return {*} JSX.Element
  */
-const withThemeProvider = (Component: NextPageWithLayout, initialColorTheme: ColorTheme) => {
+const withThemeProvider = (Component: NextPageWithLayout) => {
   return (props: any) => {
-    // const dispatch = useAppDispatch()
-    // const currentColorThemeState = useAppSelector(selectColorTheme)
-
-    const { getColorThemeStyle, getCurrentColorThemeStyle, setColorTheme, getCurrentColorThemeState } = useColorTheme()
-
-    // setColorTheme(initialColorTheme)
-    // dispatch(updateColorTheme(initialColorTheme))
-    console.log('initialColorTheme in withThemeProvider:',initialColorTheme)
-
-    const currentColorThemeState = getCurrentColorThemeState()
-    const colorThemeStyle = getColorThemeStyle(initialColorTheme || currentColorThemeState)
-    // const colorThemeStyle = getColorThemeStyle(currentColorThemeState)
-
-
-    // const theme = initialColorTheme ? getColorThemeStyle(initialColorTheme) : getCurrentColorThemeStyle()
-
-    // const themeToApply = currentColorThemeState
-
-    // const theme = initialColorTheme ? getColorThemeStyle(initialColorTheme) : getCurrentColorThemeStyle()
+    const { getCurrentColorThemeStyle } = useColorTheme()
 
     return (
-      <ThemeProvider theme={getColorThemeStyle(initialColorTheme)}>
+      <ThemeProvider theme={getCurrentColorThemeStyle()}>
         <GlobalStyle />
         <Component {...props} />
       </ThemeProvider>
@@ -77,36 +51,14 @@ const withThemeProvider = (Component: NextPageWithLayout, initialColorTheme: Col
   };
 }
 
-export default function App({ Component, pageProps, initialColorTheme }: AppPropsWithLayout) {
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  console.log('initialColorTheme in App:',initialColorTheme)
-
-  const ComponentWithThemeProvider = withThemeProvider(Component, initialColorTheme);
+  const ComponentWithThemeProvider = withThemeProvider(Component);
 
   return (
     <Provider store={store}>
       {getLayout(<ComponentWithThemeProvider {...pageProps} />)}
     </Provider>
   )
-}
-
-App.getInitialProps = async (appContext: AppContext): Promise<{pageProps: {}; initialColorTheme: ColorTheme }> => {
-  const { ctx } = appContext
-  const { req, res } = ctx
-
-  const colorThemeCookie = getColorThemeCookie({ req, res })
-  const initialColorTheme = isColorTheme(colorThemeCookie) ? colorThemeCookie : 'light'
-  console.log('initialColorTheme:', initialColorTheme)
-  let pageProps = {};
-
-  // if there are any other components that implement getInitialProps, this code is necessary. If not, remove it.
-  if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(ctx);
-  }
-
-  return {
-    pageProps,
-    initialColorTheme
-  }
 }
