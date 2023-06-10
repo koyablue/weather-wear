efa898fd039691f8e8b8ee04e10e64ec26b4d694
koyaaoyama <aoyama.ko0920@gmail.com>
2023-06-09 18:27:50 -0700

    Fix useGeocoding and getCitiesClient

--- hooks/data/useGeocoding.ts
+++ hooks/data/useGeocoding.ts
@@ -1,4 +1,4 @@
-import useSWR from 'swr'
+import useSWR, { SWRConfiguration } from 'swr'
 import { getCitiesClient } from '../../services/queries/client/getCitiesClient'
 import { GeocodingApiQuery } from '../../types/geocoding'
 
@@ -7,17 +7,14 @@ import { GeocodingApiQuery } from '../../types/geocoding'
  *
  * @param {GeocodingApiQuery} q
  * @param {number} [limit]
- * @return {*} {
-    geocodingResult: GeocodingApiResponse;
-    error: any;
-    isLoading: boolean;
-    isValidating: boolean;
-  }
+ * @param {SWRConfiguration<any, any>} [options]
+ * @return {*}
  */
-export const useGeocoding = (q: GeocodingApiQuery, limit?: number) => {
+export const useGeocoding = (q: GeocodingApiQuery, limit?: number, options?: SWRConfiguration<any, any>) => {
   const { data, error, isLoading, isValidating } = useSWR(
     ['geocoding/get', q, limit],
     () => getCitiesClient(q, limit),
+    options,
   )
 
   return {
