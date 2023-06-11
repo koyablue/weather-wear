446d6fbc72ed26535059e81d02e5882a9db6f3af
koyaaoyama <aoyama.ko0920@gmail.com>
2023-06-11 16:28:43 -0700

    Fix pages/index.tsx. Remove getServerSideProps to not do IP geolocation and weather data fetching based on the geolocation.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,15 @@
 import { ReactElement } from 'react'
-import { GetServerSideProps } from 'next'
-import { SWRConfig, unstable_serialize } from 'swr'
+import { SWRConfig } from 'swr'
 
 // components
 import BaseLayout from '../components/layouts/baseLayout'
 import Main from '../components/pages/main'
 
-// services
-import { getUserLocationServer } from '../services/queries/server/getUserLocationServer'
-import { getCurrentWeatherServer } from '../services/queries/server/getCurrentWeatherServer'
-
-// types
-import { GeolocationApiResponse } from '../types/geolocationApi'
-import { CurrentWeatherApiResponse } from '../types/weatherApi'
-
-
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const fields = []
-  let userLocation: Partial<GeolocationApiResponse> | null
-  let currentWeather: CurrentWeatherApiResponse | null
-
-  try {
-    userLocation = await getUserLocationServer(fields)
-    currentWeather = await getCurrentWeatherServer(userLocation.latitude, userLocation.longitude, 'metric')
-  } catch(error) {
-    userLocation = null
-    currentWeather = null
-  }
-
-  return {
-    props: {
-      fallback: {
-        [unstable_serialize(['userLocation/get', fields])]: userLocation,
-        [unstable_serialize(['currentWeather/get', userLocation?.latitude, userLocation?.longitude, 'metric'])]: currentWeather
-      },
-    },
-  }
-}
-
-
-type PageProps = {
-  fallback: {
-    [key: string]: any
-  }
-}
-
 // TODO: global error handling
 
-const Home = ({ fallback }: PageProps) => {
+const Home = () => {
   return (
-    <SWRConfig value={{fallback}}>
+    <SWRConfig>
       <Main />
     </SWRConfig>
   )
