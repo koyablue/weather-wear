ac02f88269bf557f659501f6a99dc93e6b4666b2
koyaaoyama <aoyama.ko0920@gmail.com>
2023-06-23 22:47:51 -0700

    Implement useGeolocation

---
+++ hooks/data/useGeolocation.ts
@@ -0,0 +1,24 @@
+import useSWR, { SWRConfiguration } from 'swr'
+import { getUserLocation } from '../../services/queries/client/getUserLocation'
+
+/**
+ * Custom hook to use geolocation api to get current location of the user
+ *
+ * @param {string} apiKey
+ * @param {SWRConfiguration<any, any>} [options]
+ * @return {*}
+ */
+export const useGeolocation = (apiKey: string, options?: SWRConfiguration<any, any>) => {
+  const { data, error, isLoading, isValidating } = useSWR(
+    ['geolocation/get', apiKey],
+    () => getUserLocation(apiKey),
+    { refreshInterval: 600000, ...options }
+  )
+
+  return {
+    userLocation: data,
+    error,
+    isLoading,
+    isValidating,
+  }
+}
---
+++ services/queries/client/getUserLocation.ts
@@ -0,0 +1,13 @@
+import { axiosBase } from '../../axiosBase'
+import { getGeolocationApiEndpoint } from '../../../utils/geolocation'
+
+/**
+ * Call geolocation API in client side code
+ *
+ * @param {string} apiKey
+ * @return {*}  {Promise<GeolocationApiResponse>}
+ */
+export const getUserLocation = async (apiKey: string): Promise<GeolocationApiResponse> => {
+  const res = await axiosBase().get(getGeolocationApiEndpoint(apiKey))
+  return res.data
+}
